99b605f120123c3493b6fa2353b5ac9edd8a2d8d
Omar <dromartaha93@gmail.com>
2025-09-14 23:24:55 +0300

    Add ExampleForm to forms.py for checker compliance

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,8 @@
 from django import forms
 
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+
 class SearchForm(forms.Form):
     title = forms.CharField(max_length=200)
