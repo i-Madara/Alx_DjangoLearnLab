6fe368bb5766021bc3184c5a8a33764bd8b66a93
Omar <dromartaha93@gmail.com>
2025-09-14 22:47:31 +0300

    Fix: register CustomUser in admin and set AUTH_USER_MODEL

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-@admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
@@ -14,3 +13,5 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'date_of_birth', 'is_staff')
     search_fields = ('username', 'email', 'first_name', 'last_name')
     list_filter = ('is_staff', 'is_superuser', 'date_of_birth')
+
+admin.site.register(CustomUser, CustomUserAdmin)
