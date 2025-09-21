6fe7e747de3708a63493c979dc4a30578151d656
Omar <dromartaha93@gmail.com>
2025-09-21 21:11:42 +0300

    Last Task

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
+from rest_framework import permissions
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -15,3 +16,8 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# BookViewSet now requires authentication.
+# Use /api-token-auth/ to get a token by sending username + password.
+# Then include "Authorization: Token <yourtoken>" in request headers.
