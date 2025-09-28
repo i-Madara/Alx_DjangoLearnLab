1d518c6a0bf2a360995f9c62566454e808b9d65b
Omar <dromartaha93@gmail.com>
2025-09-28 21:50:16 +0300

    Task 0

---
+++ advanced-api-project/api_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Author(models.Model):
+    """Represents an author. One author can have many books."""
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    """Represents a book written by an author."""
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title} ({self.publication_year})"
